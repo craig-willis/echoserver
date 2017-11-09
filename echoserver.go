ac31046e844333e9b1d260a66cb155312c53bdb3
Craig Willis <willis8@illinois.edu>
2017-11-09 12:19:48 -0600

    Added rc to echo server

--- echoserver.go
+++ echoserver.go
@@ -7,14 +7,23 @@ import "flag"
 import "time"
 import "net/http"
 import "net/http/httputil"
+import "strconv"
 
 func RequestDumpHandler(resp http.ResponseWriter, req *http.Request) {
 	request_dump, err := httputil.DumpRequest(req, true)
+
+	rc := 200
+	rcstr := req.URL.Query().Get("rc")
+	if rcstr != "" {
+		rc, err = strconv.Atoi(rcstr)
+	}
+	fmt.Printf("%d %s", rc, rcstr)
 
 	t := time.Now()
 	if err == nil {
 		reqInfo := fmt.Sprintf("\n---\n[%s]\n%s", t.Format(time.StampMilli), request_dump)
 		fmt.Println(reqInfo)
+		resp.WriteHeader(rc)
 		resp.Write([]byte(reqInfo))
 	} else {
 		msg := fmt.Sprintf("Could not handle request: %s\n", err.Error())
