71c0befe333d893a522ab6722cb6842a9d24a90c
Craig Willis <willis8@illinois.edu>
2017-11-09 12:36:59 -0600

    Added cookie

--- echoserver.go
+++ echoserver.go
@@ -4,17 +4,33 @@ package main
 import "fmt"
 import "log"
 import "flag"
-import "time"
 import "net/http"
 import "net/http/httputil"
+import "strconv"
+import "time"
 
 func RequestDumpHandler(resp http.ResponseWriter, req *http.Request) {
 	request_dump, err := httputil.DumpRequest(req, true)
+
+	rc := 200
+	rcstr := req.URL.Query().Get("rc")
+	if rcstr != "" {
+		rc, err = strconv.Atoi(rcstr)
+	}
+	fmt.Printf("%d %s", rc, rcstr)
+
+	cookie := http.Cookie{
+		Name:    "echotest",
+		Value:   "value",
+		Expires: time.Now().Add(365 * 24 * time.Hour),
+	}
+	http.SetCookie(resp, &cookie)
 
 	t := time.Now()
 	if err == nil {
 		reqInfo := fmt.Sprintf("\n---\n[%s]\n%s", t.Format(time.StampMilli), request_dump)
 		fmt.Println(reqInfo)
+		resp.WriteHeader(rc)
 		resp.Write([]byte(reqInfo))
 	} else {
 		msg := fmt.Sprintf("Could not handle request: %s\n", err.Error())
